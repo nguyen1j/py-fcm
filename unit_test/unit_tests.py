db94654047df64557316a5d9869b76b7650c28ec
Jacob Frelinger <jacob.frelinger@duke.edu>
2009-08-25 22:36:59 -0400

    fix markers being wrong from scatters being passed as a list of strings.  It should now support being passed either.

---
+++ unit_test/unit_tests.py
@@ -0,0 +1,25 @@
+"""
+Unit test framework
+"""
+
+import unittest
+from test_Annotation import FCMAnnotationTestCase
+from test_FCM_data import FCMdataTestCase
+from test_load_lmd import FCSreaderLMDTestCase
+from test_transforms import FCMtransformTestCase
+from test_load_fcs import FCSreaderTestCase
+from test_tree  import TreeTestCase
+
+
+if __name__ == "__main__":
+    suite1 = unittest.makeSuite(FCMdataTestCase,'test')
+    suite2 = unittest.makeSuite(FCMAnnotationTestCase,'test')
+    suite3 = unittest.makeSuite(FCSreaderLMDTestCase, 'test')
+    suite4 = unittest.makeSuite(FCMtransformTestCase, 'test')
+    suite5 = unittest.makeSuite(FCSreaderTestCase,'test')
+    suite6 = unittest.makeSuite(TreeTestCase, 'test')
+    alltests = unittest.TestSuite((suite1, suite2, suite3, suite4, suite5, suite6))
+
+    unittest.main()
+
+        
