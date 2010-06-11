1a1aee8d6c841aac6abb8a80d10c9679dfe7b9f1
Jacob Frelinger <jacob.frelinger@duke.edu>
2010-06-11 15:19:52 -0400

    add tracking of modes in modal clusters.

---
+++ src/core/subsample.py
@@ -0,0 +1,59 @@
+'''
+Created on Aug 27, 2009
+
+@author: jolly
+'''
+from util import SubsampleNode
+from util import DropChannelNode
+
+
+class Subsample(object):
+    '''
+    Takes a slice object and performs subsampling on the fcm object.
+    '''
+
+
+    def __init__(self, slicing):
+        '''
+        slicing = tuple of slices
+        '''
+        self.samp = slicing
+        
+    def subsample(self, fcm):
+        node = SubsampleNode("", fcm.get_cur_node(), self.samp)
+        fcm.add_view(node)
+        return fcm
+
+class _SubsampleFactory(object):
+    '''
+    factory generator of subsample objects
+    '''
+    
+    def __init__(self):
+        pass
+    
+    def __getitem__(self, item):
+        return Subsample(item)
+    
+SubsampleFactory = _SubsampleFactory()
+
+
+class DropChannel(object):
+    """
+    Drop channels by name from a fcm view
+    """
+    def __init__(self, idxs):
+        self.idxs = idxs
+        
+    def drop(self, fcm):
+        channels = fcm.channels[:]
+        for i in self.idxs:
+            channels.remove(i)
+        left = []
+        for i in channels:
+            left.extend(fcm.name_to_index(i))
+            
+        left.sort()
+        node = DropChannelNode("", fcm.get_cur_node(), left)
+        fcm.add_view(node)
+        return fcm
